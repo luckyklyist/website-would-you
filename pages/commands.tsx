bc0f2c77f9915a3e1049ffeaae06aed31c6162f2
Marc <71778381+MarcWebDev@users.noreply.github.com>
2023-03-01 20:46:28 +0100

    feat: Verified/Partnered Badge in Server Slider

--- pages/commands.tsx
+++ pages/commands.tsx
@@ -1,8 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import Footer from "@/components/Footer";
+import { useState } from "react";
+import commands from "../data/commands.json";
 
 export default function Commands() {
+  const [openedCommand, setOpenedCommand] = useState("");
+
   return (
     <>
       <Head>
@@ -21,36 +25,47 @@ export default function Commands() {
         <h3>All The Commands Would You Has To Offer!</h3>
 
         <div className="commands">
-          <div className="command">
-            <div className="command-head">
-              <div className="command-head-left">
-                <h4>Custom</h4>
-                <p>Sends a custom would you message</p>
+          {commands.commands.map((c: any) => (
+            <div
+              className={`command ${
+                openedCommand === c.name ? "active" : ""
+              }`}
+              onClick={() =>
+                openedCommand === c.name
+                  ? setOpenedCommand("")
+                  : setOpenedCommand(c.name)
+              }
+            >
+              <div className="command-head">
+                <div className="command-head-left">
+                  <h4>{c.name}</h4>
+                  <p>{c.description}</p>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="none"
+                  viewBox="0 0 30 30"
+                >
+                  <path
+                    fill="#666"
+                    d="M15 20.938a.93.93 0 0 1-.663-.275l-8.75-8.75a.938.938 0 1 1 1.327-1.327L15 18.674l8.088-8.088a.938.938 0 1 1 1.326 1.327l-8.75 8.75a.94.94 0 0 1-.665.274Z"
+                  />
+                </svg>
               </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="30"
-                fill="none"
-                viewBox="0 0 30 30"
-              >
-                <path
-                  fill="#666"
-                  d="M15 20.938a.93.93 0 0 1-.663-.275l-8.75-8.75a.938.938 0 1 1 1.327-1.327L15 18.674l8.088-8.088a.938.938 0 1 1 1.326 1.327l-8.75 8.75a.94.94 0 0 1-.665.274Z"
-                />
-              </svg>
+              <div className="command-expand">
+                <div className="command-expand-group">
+                  <h5>Usage</h5>
+                  <h6>{c.usage}</h6>
+                </div>
+                <div className="command-expand-group">
+                  <h5>Subcommands</h5>
+                  <h6>{c.subcommands ? c.subcommands : 'None'}</h6>
+                </div>
+              </div>
             </div>
-            <div className="command-expand">
-              <div className="command-expand-group">
-                <h5>Usage</h5>
-                <h6>{"/dailymsg <subcommand>"}</h6>
-              </div>
-              <div className="command-expand-group">
-                <h5>Subcommands</h5>
-                <h6>{"channel, timezone, message, role, type"}</h6>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </main>
 
