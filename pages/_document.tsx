203a7379152e160f088159e1126a23fd9511a37b
Marc <71778381+marchoffmann07@users.noreply.github.com>
2023-03-10 23:39:44 +0100

    impr: Meta Tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Would you is a fun little bot for every server! Provide your server with fun little would you questions!"
+        />
+        <meta property="og:title" content="Would You | Discord Bot" />
+        <meta
+          property="og:description"
+          content="Would you is a fun little bot for every server! Provide your server with fun little would you questions!"
+        />
+        <meta property="og:image" content="/static/img/round.webp" />
+        <meta name="theme-color" content="#0598F6" />
+        <meta name="msapplication-TileColor" content="#0598F6" />
+      </Head>
       <body>
         <Main />
         <NextScript />
