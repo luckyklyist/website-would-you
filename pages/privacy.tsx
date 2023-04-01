00fc2a0f47003b17031c4517775acac1863a1a69
Dominik K <dominik@koch-bautechnik.de>
2023-04-01 16:48:32 +0200

    Update privacy.tsx

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -11,73 +11,116 @@ export default function Privacy() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Logo.png" />
       </Head>
-
       <Navbar />
-
       <main className="imprint-main">
         <h1>
           Privacy <span>Policy</span>
         </h1>
         <div className="imprint-content">
-          <h3>Data Usage</h3>
-          <p>
-            The bot may use stored data, as defined below, for different
-            features including but not limited to: Custom Would You Messages,
-            Guild IDs, Language Selection, Internal Settings. No usage of the
-            data outside of the aforementioned cases will happen, and the data
-            is not shared with any third-party site or service.
-          </p>
 
-          <h3>Stored Information</h3>
-          <p>
-            The bot may store the following information automatically when a
-            command is run in a new Discord ServerServer name and icon may be
-            used in log channels along with the users profile picture and
-            username after voting. Said channels are publicly avalible to
-            everyone. But no data is stored on our servers.Custom Would You
-            Messages, Guild IDs, Language Selection, Internal SettingsNo other
-            information outside of the above mentioned one will be stored.
-          </p>
+        <h3>Last updated and effective: April 1, 2023</h3>
+        <br />
+        <p>
+        Thank you for choosing to be part of our community at Would You("we," "us," or "our"). We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice or our practices with regard to        your personal information, please contact us at dominik@wouldyoubot.com.
 
-          <h3>Updating Data</h3>
-          <p>
-            The data may be updated when using specific commands. Updating data
-            will require the input of an end user, and data that can be seen as
-            sensitive, such as content of a message, may need to be stored when
-            using certain commands.No other actions may update the stored
-            information at any given time.
-          </p>
+        This privacy notice describes how we might use your information if you:
 
-          <h3>Temporarily stored Information</h3>
-          <p>
-            The Bot may store information temporarily in cache for a period of a
-            maximum of 30 days. After this time period, the cached information
-            will be dropped and only be re-added when required.Data may be
-            dropped from cache pre-maturely through actions such as removing the
-            bot from the Server or the bot restarting.
-          </p>
+        - Visit our website at [https://wouldyoubot.gg](https://wouldyoubot.gg) or https://wouldyoubot.com
+        - Engage with us in other related ways — including any sales, marketing, or events.
 
-          <h3>Removal of Data</h3>
-          <p>
-            If your want to get your data removed dm the founders
-            ForGetFulSkyBro#9999 and Dominik#5555 to get all your data wiped.
-          </p>
+        In this privacy notice, if we refer to:
 
-          <h3>Manual removal</h3>
-          <p>
-            Manual removal of the data can be requested through email at
-            dominik@wouldyoubot.com or as mentioned above a dm to the founders.
-            For security reasons, we will ask you to provide us with proof of
-            ownership of the server. It is only the server&apos;s owner that may
-            request manual removal and requesting it will result in the bot
-            being removed from the server!
-          </p>
+        - "Website," we are referring to any website of ours that references or links to this policy.
+        - "Services," we are referring to our Website, and other related services, including any sales, marketing, or events.
+
+        The purpose of this privacy notice is to explain to you in the clearest way possible what information we collect, how we use it, and what rights you have in relation to it. If there are any terms in this privacy notice that you do not agree with, please discontinue         use of our Services immediately.
+        </p>
+        <h3> PRIVACY STATEMENT</h3>
+        <p>
+        We respect the privacy of your information. We provide this explanation about our information practices as a show of our commitment to protect your privacy. This policy describes the types of information we may collect from you or that you may provide when you visit        this website and our practices for collecting, using, maintaining, protecting and disclosing that information.
+
+        This policy applies to information we collect:
+
+        On this website and/or via our Bot:
+
+        - Your discord user id, avatar and custom questions uploaded to our marketplace by the user.
+        - Questions you liked or shared on our marketplace.
+        - Data about your discord server including your server id, avatar, settings, webhook urls, custom uploaded messages and
+        - If and when available, through mobile or desktop applications you download from this website, which provide dedicated non-browser- based interaction between you and this website.
+        - When you interact with our applications on social media or third-party applications, websites and services, if those applications or advertising including links to this policy or of information from social media or third parties.
+
+        It does not apply to information collected by:
+
+        - us offline or through any other means, including on any other website operated by Would You or any third party;
+
+            or
+
+        - any third party, including through any application or content (including advertising) that may link to or be accessible from or on the website.
+
+        Please read this policy carefully to understand our policies and practices regarding your information and how we treat it. If you do not agree with our policies and practices, your choice is not to use our website or bot. By accessing or using this website/bot, you         agree to this privacy policy. This policy may change from time to time. Your continued use of this website after we make changes is deemed to be acceptance of those changes, so please check the policy every once in a while for updates.
+        </p>
+        <h3> What are your Privacy rights?</h3>
+        <p>
+        In some regions (like the PEA and UK), you have certain rights under applicable data protection laws. These may include the right (i) to request access and obtain a copy of your personal information, (ii) to request rectification or erasure; (Hi) to restrict the        processing of your personal information; and (iv) If applicable, to data portability. In certain circumstances, you may also have the right to object to the processing of your personal Inform. To make such a request please use the contact details provided below. We       will consider and act upon any request in accordance with applicable data protection laws. If we are relying on your consent to process your personal information, you have the right to withdraw your consent at any time. Please note however that this will not affect       the lawfulness of the processing before its withdrawal, nor will it affect the processing of your personal information conducted in reliance on lawful processing grounds other than consent. If you are a resident in the EEA or UK and you believe we are unlawfully      processing your personal Information, you also have the right to complain to your local data protection supervisory authority. You can find their contact details here: [https://ec.europa.eugustice/data-protectun/bodies/authontlesfindex_en.htm](https://ec.europa.       eugustice/data-protectun/bodies/authontlesfindex_en.htm). If you are a resident in Switzerland, the contact details for the data protection authorities are available here: [https://www.edoeb.admin.ch/edoebtenfhome.html](https://www.edoeb.admin.ch/edoebtenfhome.html).     </p>
+        <h3> THE INFORMATION WE COLLECT AND HOW WE COLLECT IT</h3>
+        <p>
+        Personally Identifiable Information. On our website, we only collect personally identifiable information from individuals that they provide to us voluntarily. This means we do not require you to register or provide information to us in order to view our website.        Would You only gathers personally identifiable data through our website, such as an email address, when voluntarily submitted by a visitor. Once collected, we may combine this information with other information collected from external sources.
+
+        Tools
+
+        - [Cookies (or browser cookies)](https://en.wikipedia.org/wiki/HTTP_cookie). You may refuse to accept browser cookies by activating the appropriate setting on your browser. However, if you select this setting you may be unable to access certain parts of our website.        Unless you have adjusted your browser setting so that it will refuse cookies, our system will issue cookies when you direct your browser to our website.
+        </p>
+        <h3> THIRD-PARTY WEBSITES </h3>
+        <p>
+        This website or messages distributed by our bot may contain links to third-party websites. These linked websites are not under our control, and we are not responsible for the privacy practices or the contents of any such linked website or any link contained in any        linked website. We provide such links only as a convenience, and the inclusion of a link on the website does not imply endorsement of the linked website by Would You. If you provide any personal data through any such third-party website, your transaction will occur       on the third party’s website (not this website) and the personal data you provide will be collected by and controlled by the privacy policy of that third party. We recommend that you familiarize yourself with the privacy policies and practices of any third parties.       Please note that this privacy policy does not address the privacy or Information practices of any third party website or tool.
+
+        THE WAY WE USE INFORMATION
+
+        This Privacy Statement governs use of the information that you provide to us through the website or bot. It does not govern the manner in which we may use information obtained from other sources (such as public records) or from you by means other than this website.         We and other entities that we are involved in, or provide support for, the operation of this website or other purposes use information that we collect about you or that you provide to us, including any personal information:
+
+        - To present our website and its contents to you.
+        - To provide you with information on programs, services or products.
+        - For you to register for events, promotions, contests, sweepstakes or services.
+        - To process online transactions, purchases, registrations, donations or participation in programs.
+        - To provide you with information that may be targeted to your interests.
+        - To fulfill any other purpose for which you provide it.
+        - To carry out our obligations and enforce our rights arising from any contracts entered into between you and us.
+        - To request additional information from you for various purposes.
+        - In any other way we may describe when you provide the information.
+        - For any other purpose with your consent.
+
+        We may also use your information if provided to us to contact you about our own and third-party services and products that may be of interest to you. If you do not want us to use your information in this way, you can opt out of receiving this information by so        indicating in your registration/preferences file or by notifying us
+
+        We may also use or disclose information to resolve disputes, investigate problems or enforce our Terms of Service. At times, we may review status or activity of multiple users to do so. We may disclose or access information whenever we believe in good faith that the        law so requires or if we otherwise consider it necessary to do so to maintain service and improve our services. We use your IP address to help diagnose problems with our server, to manage our website and to enhance our site based on the usage pattern we receive.
+        </p>
+        <h3> Controls for Do-Not-Track features.</h3>
+        <p>
+        We do not track you.
+        </p>
+        <h3> Do California residents have specific privacy rights?</h3>
+        <p>
+        California Civil Code Section 1798.83, also known as the "Shine The Light. law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to         third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal Information in the Immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit        your request in writing to us using the contact Information provided below. If you are under 18 years of age, reside in California, and have a registered account with the Website, you have the right to request removal of unwanted data that you publicly post on the        Website. To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside In California. We will make sure the data is not publicly displayed on       the Website, but please be aware that the data may not be completely or comprehensively removed from all our systems (e.g. backups etc.).
+        </p>
+        <h3> Do we make updates to this notice?</h3>
+        <p>
+        We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either         by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
+        </p>
+        <h3> How can you contact us about this notice?</h3>
+        <p>
+        If you have any questions or comments about this notice, you may email us at dominik@wouldyoubot.com.
+        </p>
+        <h3> How can you review, update, or delete the data we collect from you?</h3>
+        <p>
+        Based on the applicable laws of your country, you may have the right to request access to the personal information we collect from you, change that information, or delete it in some circumstances. To request to review, update, or delete your personal Information,         please visit: <a href="https://wouldyoubot.gg/discord" target={"_blank"}>https://wouldyoubot.gg/discord</a>.
+        </p>
+        <h3>Attribution</h3>
+        <p>
+        Thanks to <a href="https://scrumpbot.com/" target={"_blank"}>https://scrumpbot.com/</a> for help on this privacy policy.</p>
           <br />
           <br />
           <br />
         </div>
       </main>
-
       <Footer />
     </>
   );
