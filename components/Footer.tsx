14fe1bf597fdf7ba335be98eee41d3bcb6012431
Podskio <podskio1@gmail.com>
2023-08-27 23:44:25 -0700

    Move Navbar and Footer to tw

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,34 +1,39 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-head">
-        <div className="footer-head-column extend">
-          <div className="logo">
-            <Image src="/Logo.svg" alt="Logo"  className="rounded-full" draggable={false} width="50" height="50"/>
-            <p>WouldYou</p>
+      <div className="mb-8 flex flex-col items-center justify-between text-neutral-300 sm:flex-row">
+        <div className="flex flex-1 flex-col items-center sm:items-start">
+          <div className="flex items-center text-xl font-semibold text-white">
+            <Image
+              src="/Logo.svg"
+              alt="Logo"
+              className="rounded-full"
+              width="40"
+              height="40"
+            />
+            <p className="ml-3">Would You</p>
           </div>
-          <p className="logo-subtext">
-            Would You is not affiliated with Discord or Reddit.
+          <p className="mt-4 text-sm">
+            &copy; {new Date().getFullYear()} Dominik Development. All rights
+            reserved.
           </p>
         </div>
-        <div className="footer-head-column-wrapper">
-          <div className="footer-head-column">
-            <h4>Recommended</h4>
+        <div className="mt-8 flex items-start gap-16 whitespace-nowrap sm:mt-0">
+          <div className="flex flex-1 flex-col items-center sm:items-start">
+            <h4 className="mb-2 text-lg font-semibold text-white">
+              Recommended
+            </h4>
             <Link href="/">Home</Link>
             <Link href="/commands">Commands</Link>
-            <Link href="/reddit" target="_blank">Reddit</Link>
+            <Link href="/reddit" target="_blank">
+              Reddit
+            </Link>
           </div>
-          <div className="footer-head-column">
-            <h4>Legal</h4>
-            <Link href="/legal">Legal Notice</Link>
-            <Link href="/privacy">Privacy Policy</Link>
-            <Link href="/terms">Terms of Service</Link>
-          </div>
-          <div className="footer-head-column">
-            <h4>Links</h4>
+          <div className="flex flex-1 flex-col items-center sm:items-start">
+            <h4 className="mb-2 text-lg font-semibold text-white">Links</h4>
             <Link href="/discord" target={"_blank"}>
               Support Server
             </Link>
@@ -38,21 +43,34 @@ const Footer = () => {
             <Link href="/vote" target={"_blank"}>
               Vote
             </Link>
-           </div>
+          </div>
+          <div className="flex flex-1 flex-col items-center sm:items-start">
+            <h4 className="mb-2 text-lg font-semibold text-white">Legal</h4>
+            <Link href="/legal">Legal Notice</Link>
+            <Link href="/privacy">Privacy Policy</Link>
+            <Link href="/terms">Terms of Service</Link>
+          </div>
         </div>
       </div>
-      <div className="footer-copyright">
-        <hr />
-        <p>
-          Made With ♥ By{" "}
-          <Link href="https://github.com/MarcWebDev" target={"_blank"}>
-            MarcDev
+      <div>
+        <hr className="border border-neutral-800" />
+        <p className="mt-8 text-center text-sm text-neutral-300">
+          Made with <span className="text-brand-red-100">♥</span> by{" "}
+          <Link
+            href="https://github.com/orgs/Would-You-Bot/people"
+            target={"_blank"}
+            className="text-white underline"
+          >
+            Would You Team
           </Link>{" "}
           &{" "}
-          <Link href="https://github.com/mezotv" target={"_blank"}>
-            Dominik
-          </Link>{" "}
-          For WouldYouBot.gg{" "}
+          <Link
+            href="https://github.com/Would-You-Bot/website/graphs/contributors"
+            target={"_blank"}
+            className="text-white underline"
+          >
+            Contributors
+          </Link>
         </p>
       </div>
     </footer>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useState } from "react";
 import { motion, useAnimationControls } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Button from "./Button";
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -10,97 +11,126 @@ const Navbar = () => {
   const lineThreeControls = useAnimationControls();
   const menuControls = useAnimationControls();
 
-  function toggleMobileMenu() {
+  const toggleMobileMenu = () => {
     if (mobileMenu) {
       lineOneControls.start(
         { rotate: "0deg" },
-        { duration: 0.3, type: "spring" }
+        { duration: 0.3, type: "spring" },
       );
       lineTwoControls.start(
         { opacity: 1, transform: "translateX(0)" },
-        { duration: 0.12 }
+        { duration: 0.12 },
       );
       lineThreeControls.start(
         { rotate: "0deg" },
-        { duration: 0.3, type: "spring" }
+        { duration: 0.3, type: "spring" },
       );
       menuControls.start(
         { opacity: 0.5, left: "100vw", pointerEvents: "none" },
-        { duration: 0.21, type: "spring" }
+        { duration: 0.21, type: "spring" },
       );
 
       setMobileMenu(false);
     } else {
       lineOneControls.start(
         { rotate: "45deg" },
-        { duration: 0.3, type: "spring" }
+        { duration: 0.3, type: "spring" },
       );
       lineTwoControls.start(
         { opacity: 0, transform: "translateX(1)" },
-        { duration: 0.12 }
+        { duration: 0.12 },
       );
       lineThreeControls.start(
         { rotate: "-45deg" },
-        { duration: 0.3, type: "spring" }
+        { duration: 0.3, type: "spring" },
       );
       menuControls.start(
         { opacity: 1, left: "0", pointerEvents: "all" },
-        { duration: 0.21, type: "spring" }
+        { duration: 0.21, type: "spring" },
       );
 
       setMobileMenu(true);
     }
-  }
+  };
 
   return (
-    <nav>
-      <div className="nav-left">
+    <nav className="fixed left-0 top-0 z-50 flex h-28 w-full items-center justify-between border-b border-b-neutral-800 bg-neutral-900 bg-opacity-90 backdrop-blur-sm">
+      <div className="ml-8 flex items-center sm:ml-[17vw]">
         <Link href="/">
-          <div className="logo">
-            <Image src="/Logo.svg"  className="rounded-full" alt="Would You Logo" draggable={false} width="50" height="50" />
-            <p>Would You</p>
+          <div className="flex items-center">
+            <Image
+              src="/Logo.svg"
+              className="rounded-full"
+              alt="Would You Logo"
+              width="50"
+              height="50"
+            />
+            <p className="ml-4 text-2xl font-semibold text-white">Would You</p>
           </div>
         </Link>
-        <div className="nav-items">
-          <Link href="/">Home</Link>
-          <Link href="/commands">Commands</Link>
+        <div className="ml-24 hidden items-center sm:flex">
+          <Link
+            href="/"
+            className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+          >
+            Home
+          </Link>
+          <Link
+            href="/commands"
+            className="mr-6 text-lg text-neutral-300 transition-all hover:text-neutral-100"
+          >
+            Commands
+          </Link>
         </div>
       </div>
-      <div className="nav-right">
-        <Link href="/discord" target={"_blank"}>
-          <button className="wy-button primary">Support</button>
+      <div className="z-50 mr-8 flex items-center sm:mr-[17vw]">
+        <Link href="/discord" target="_blank" className="hidden sm:block">
+          <Button className="">Support</Button>
         </Link>
-        <div className="menu-icon" onClick={() => toggleMobileMenu()}>
+        <div
+          className="relative ml-6 flex h-6 w-8 flex-col items-center justify-between sm:hidden"
+          onClick={() => toggleMobileMenu()}
+        >
           <motion.span
-            className="menu-line line-1"
+            className="h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
             initial={{ rotate: "0deg" }}
             animate={lineOneControls}
-          ></motion.span>
+          />
           <motion.span
-            className="menu-line line-2"
-            initial={{ opacity: 1, transform: "translateX(0)" }}
+            className="menu-line line-1 h-[3px] w-[30px] rounded-full bg-neutral-300"
+            initial={{ opacity: 1 }}
             animate={lineTwoControls}
-          ></motion.span>
+          />
           <motion.span
-            className="menu-line line-3"
+            className="menu-line line-1 h-[3px] w-[30px] origin-left rounded-full bg-neutral-300"
             initial={{ rotate: "0deg" }}
             animate={lineThreeControls}
-          ></motion.span>
+          />
         </div>
       </div>
-      <hr />
 
       <motion.div
-        className="nav-mobile-menu"
+        className="fixed left-0 top-0 z-40 h-[100vh] w-[100vw] bg-neutral-900"
         transition={{ duration: 0.21, type: "easeInOut" }}
         initial={{ opacity: 0.5, left: "100vw", pointerEvents: "none" }}
         animate={menuControls}
       >
-        <div className="links">
-          <Link href="/">Home</Link>
-          <Link href="/commands">Commands</Link>
-          <Link href="/discord" target={"_blank"}>
-            <button className="wy-button primary">Support</button>
+        <div className="absolute top-36 flex w-full flex-col items-center">
+          <Link href="/" className="mt-8 text-center text-3xl text-white">
+            Home
+          </Link>
+          <Link
+            href="/commands"
+            className="mt-8 text-center text-3xl text-white"
+          >
+            Commands
+          </Link>
+          <Link
+            href="/discord"
+            target="_blank"
+            className="mt-8 text-center text-2xl"
+          >
+            <Button>Support</Button>
           </Link>
         </div>
       </motion.div>
