cb1159a7c14685062cf6132e432ed3f446155482
Podskio <podskio1@gmail.com>
2023-08-27 22:46:57 -0700

    CVA and button component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
 
 module.exports = {
   content: [
@@ -10,9 +10,13 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          blue: "#0598F4",
-          red: "#F00605",
+          blue: { 100: "#0598F4", 200: "#037ac7", 300: "#016cb0" },
+          red: { 100: "#F00605", 200: "#b60505", 300: "#9a0404" },
         },
+      },
+      dropShadow: {
+        ["red-glow"]: ["2px 2px 20px #f00505"],
+        ["blue-glow"]: ["2px 2px 20px #0598f6"],
       },
     },
   },
