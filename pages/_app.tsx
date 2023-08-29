93b36953388a855076ca637cbd42ab8cb802d5da
Podskio <podskio1@gmail.com>
2023-08-28 22:20:31 -0700

    Add nav and footer to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import "@/styles/index.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Would You</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/Logo.png" />
+      </Head>
+
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
