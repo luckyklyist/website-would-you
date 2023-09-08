7f8f21f7d613cc7c75d9f8679e2bd71c7c4d0c82
Podskio <podskio1@gmail.com>
2023-09-08 09:45:00 -0700

    Footer spacing fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="mt-8 py-8 xl:px-[17vw]">
+    <footer className="mt-8 px-8 py-8 xl:px-[17vw]">
       <div className="mb-8 flex flex-col items-center justify-between gap-4 text-neutral-300 md:flex-row">
         <div className="flex w-full flex-col items-center md:mr-4 md:items-start">
           <div className="flex items-center text-xl font-bold text-white">
