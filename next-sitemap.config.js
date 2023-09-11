9fc15ac655918a78018d8ee0157bd64729a0d46b
Podskio <podskio1@gmail.com>
2023-09-11 16:21:09 -0700

    Add blog posts to sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://wouldyoubot.gg/',
-    generateRobotsTxt: true,
-  }
+const { readdirSync } = require("fs");
+const path = require("path");
+
+/** @type {import('next-sitemap').IConfig} */ module.exports = {
+  siteUrl: "https://wouldyoubot.gg/",
+  generateRobotsTxt: true,
+
+  // Add blog post paths, since they are loaded from an external directory
+  additionalPaths: async () =>
+    readdirSync(path.join(process.cwd(), "posts"))
+      .filter((path) => path.charAt(0) !== "_")
+      .filter((path) => /\.mdx?$/.test(path))
+      .map((path) => `/blog/${path.replace(/\.mdx?$/, "")}`),
+};
