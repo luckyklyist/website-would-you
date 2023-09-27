72df4a249ca15a3b73737de82efa063508b04d8f
Podskio <podskio1@gmail.com>
2023-09-26 20:23:07 -0700

    Remove dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,6 @@ import {
   DiscordReply,
 } from "@skyra/discord-components-react";
 import { motion } from "framer-motion";
-import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -567,7 +566,7 @@ const Home = () => {
         />
       </section>
 
-      <section className="mt-36 bg-neutral-950 py-12 px-9 xl:px-[17vw]">
+      <section className="mt-36 bg-neutral-950 px-9 py-12 xl:px-[17vw]">
         <motion.h2
           initial={{ opacity: 0, transform: "translateY(10px)" }}
           whileInView={{ opacity: 1, transform: "translateY(0)" }}
@@ -605,4 +604,4 @@ const Home = () => {
   );
 };
 
-export default dynamic(() => Promise.resolve(Home), { ssr: false });
+export default Home;
